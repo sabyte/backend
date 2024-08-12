63ba558fd1a0a6cbff48e1640655d1d23e45195c
Md. Sabit Shahriar Haque <69699980+sabyte@users.noreply.github.com>
2024-08-12 07:19:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ app.post('/grant-token', async (req, res) => {
 
     try {
         const response = await axios.post('https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized/checkout/token/grant', {
-            app_key,
-            app_secret
+            app_key: 'Pc0yKAFRbzf6N3yk9msFYs8Ttc',
+            app_secret: 'LNHBzWWQliD4uLzxvSRNHFNFFUrIeCTptabBuNIPtAIfDKVEbK0c'
         }, {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                'username': username,
-                'password': password
+                'username': '01619754538',
+                'password': '-2E8SmJIB{t'
             },
         });
 
@@ -35,6 +35,36 @@ app.post('/grant-token', async (req, res) => {
     } catch (error) {
         console.error('Error getting access token:', error.response ? error.response.data : error.message);
         res.status(500).json({ message: 'Error getting access token', error: error.response ? error.response.data : error.message });
+    }
+});
+
+// Create Payment Route
+app.post('/create-payment', async (req, res) => {
+    const { id_token } = req.body;
+
+    try {
+        const response = await axios.post('https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized/checkout/create', {
+            mode: '0011',
+            payerReference: '01723888888',
+            callbackURL: 'https://vending-user-interface-frontend.onrender.com',
+            merchantAssociationInfo: 'MI05MID54RF09123456One',
+            amount: '500',
+            currency: 'BDT',
+            intent: 'sale',
+            merchantInvoiceNumber: 'Inv0124'
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${id_token}`,
+                'X-App-Key': 'Pc0yKAFRbzf6N3yk9msFYs8Ttc'
+            },
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error creating payment:', error.response ? error.response.data : error.message);
+        res.status(500).json({ message: 'Error creating payment', error: error.response ? error.response.data : error.message });
     }
 });
 
