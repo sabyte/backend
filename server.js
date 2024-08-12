7eb48eac146cec992da17f7fc6bd66c3868e59e7
Md. Sabit Shahriar Haque <69699980+sabyte@users.noreply.github.com>
2024-08-12 08:13:55 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set up middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
 
-// Root Route
-app.get('/', (req, res) => {
-    res.send('Backend is running!');
-});
+// Sensitive information
+const username = '01619754538';
+const password = '-2E8SmJIB{t';
+const app_key = 'Pc0yKAFRbzf6N3yk9msFYs8Ttc';
+const app_secret = 'LNHBzWWQliD4uLxzvSRNHFNFFUrIeCTptabBuNIPtAIfDKVEbK0c';
 
 // Grant Token Route
 app.post('/grant-token', async (req, res) => {
-    const { app_key, app_secret, username, password } = req.body;
-
     try {
         const response = await axios.post('https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized/checkout/token/grant', {
-            app_key: 'Pc0yKAFRbzf6N3yk9msFYs8Ttc',
-            app_secret: 'LNHBzWWQliD4uLzxvSRNHFNFFUrIeCTptabBuNIPtAIfDKVEbK0c'
+            app_key: app_key,
+            app_secret: app_secret
         }, {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                'username': 'username',
-                'password': 'password'
+                'username': username,
+                'password': password
             },
         });
 
@@ -38,36 +38,12 @@ app.post('/grant-token', async (req, res) => {
     }
 });
 
-// Create Payment Route
-app.post('/create-payment', async (req, res) => {
-    const { id_token } = req.body;
-
-    try {
-        const response = await axios.post('https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized/checkout/create', {
-            mode: '0011',
-            payerReference: '01723888888',
-            callbackURL: 'https://vending-user-interface-frontend.onrender.com',
-            merchantAssociationInfo: 'MI05MID54RF09123456One',
-            amount: '500',
-            currency: 'BDT',
-            intent: 'sale',
-            merchantInvoiceNumber: 'Inv0124'
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${id_token}`,
-                'X-App-Key': 'Pc0yKAFRbzf6N3yk9msFYs8Ttc'
-            },
-        });
-
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error creating payment:', error.response ? error.response.data : error.message);
-        res.status(500).json({ message: 'Error creating payment', error: error.response ? error.response.data : error.message });
-    }
+// Root route to confirm the server is running
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
